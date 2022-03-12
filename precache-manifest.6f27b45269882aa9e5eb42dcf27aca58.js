139d7edac5a343aa4bd05f0c23a0889d7dabaf5f
Aze Pelayo <azriellepelayo@gmail.com>
2022-03-12 20:11:15 +0800

    Updates

---
+++ precache-manifest.6f27b45269882aa9e5eb42dcf27aca58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6872cfe0f9d32670944c4af8fd6e81",
+    "url": "/quizzap/index.html"
+  },
+  {
+    "revision": "2718b0148ac7c1ef0702",
+    "url": "/quizzap/static/css/main.180491b9.chunk.css"
+  },
+  {
+    "revision": "7ae61681938c1e5c359e",
+    "url": "/quizzap/static/js/2.2fb1932b.chunk.js"
+  },
+  {
+    "revision": "e5b4c8cb5e9fc5a7cd67b0ab73a901d9",
+    "url": "/quizzap/static/js/2.2fb1932b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2718b0148ac7c1ef0702",
+    "url": "/quizzap/static/js/main.c72e87b1.chunk.js"
+  },
+  {
+    "revision": "032edd12c327a23557c2",
+    "url": "/quizzap/static/js/runtime-main.47b45ad8.js"
+  }
+]);
